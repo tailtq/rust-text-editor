fab9f68b796a2a3f466f3965150e3f39378438b8
Tai Le <ltquoctaidn98@gmail.com>
2025-10-29 22:31:24 +0700

    feat: update

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,8 +1,19 @@
-use std::io::stdout;
-use crossterm::execute;
-use crossterm::cursor::MoveTo;
+use std::io::{stdout, Write};
+use crossterm::style::Print;
+use crossterm::{queue};
+use crossterm::cursor::{MoveTo, Hide, Show};
 use crossterm::terminal::{ClearType, disable_raw_mode, enable_raw_mode, Clear, size};
 
+
+pub struct Size {
+    pub height: u16,
+    pub width: u16,
+}
+
+pub struct Position {
+    pub x: u16,
+    pub y: u16,
+}
 
 pub struct Terminal {
 }
@@ -11,21 +22,38 @@ impl Terminal {
     pub fn initialize() -> Result<(), std::io::Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(0, 0)?;
+        Self::move_cursor_to(Position{ x: 0, y: 0 })?;
+        Self::execute()?;
         Ok(())
     }
     pub fn terminate() -> Result<(), std::io::Error> {
+        Self::execute()?;
         disable_raw_mode()
     }
     pub fn clear_screen() -> Result<(), std::io::Error> {
-        let mut stdout = stdout();
-        execute!(stdout, Clear(ClearType::All))
+        queue!(stdout(), Clear(ClearType::All))
     }
-    pub fn move_cursor_to(x: u16, y: u16) -> Result<(), std::io::Error> {
-        execute!(stdout(), MoveTo(x, y))?;
+    pub fn move_cursor_to(pos: Position) -> Result<(), std::io::Error> {
+        queue!(stdout(), MoveTo(pos.x, pos.y))?;
         Ok(())
     }
-    pub fn size() -> Result<(u16, u16), std::io::Error> {
-        size()
+    pub fn size() -> Result<Size, std::io::Error> {
+        let (width, height) = size()?;
+        Ok(Size{ width: width, height: height })
+    }
+    pub fn print(string: &str) -> Result<(), std::io::Error> {
+        queue!(stdout(), Print(string))
+    }
+    pub fn show_cursor() -> Result<(), std::io::Error> {
+        queue!(stdout(), Show)
+    }
+    pub fn hide_cursor() -> Result<(), std::io::Error> {
+        queue!(stdout(), Hide)
+    }
+    pub fn clear_line() -> Result<(), std::io::Error> {
+        queue!(stdout(), Clear(ClearType::CurrentLine))
+    }
+    pub fn execute() -> Result<(), std::io::Error> {
+        stdout().flush()
     }
 }
